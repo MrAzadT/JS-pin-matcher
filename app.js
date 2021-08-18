83254c139e160aabd5a72bb18b1aaab88997dc86
azadtamal <mrazadt@gmail.com>
2021-08-18 21:14:32 +0600

    pin

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function generatePin() {
-  let num = Math.floor(1000 + Math.random() * 10000);
+  let num = Math.floor(1000 + Math.random() * 9000);
 
   let firstInput = document.getElementById("firstInput");
   firstInput.value = num;
   return firstInput;
 }
+
+document.getElementById("calculatorNumbers").addEventListener("click", (e) => {
+  const pinCode = e.target.innerText;
+  const secondInput = document.getElementById("secondInput");
+  const newPin = secondInput.value;
+  const newPinCode = newPin + pinCode;
+  secondInput.value = newPinCode;
+});
