9590445080d8bc91a8df1015be7cc90d3bacbf34
azadtamal <mrazadt@gmail.com>
2021-08-18 23:05:18 +0600

    pin

--- app.js
+++ app.js
@@ -1,54 +1,42 @@
-// function generatePin() {
-//   let num = Math.floor(1000 + Math.random() * 9000);
+function generatePin() {
+  let num = Math.floor(1000 + Math.random() * 9000);
 
-//   const firstInput = document.getElementById("firstInput");
-//   firstInput.value = num;
-//   return firstInput;
-// }
+  const firstInput = document.getElementById("firstInput");
+  firstInput.value = num;
+  return firstInput;
+}
 
-// document.getElementById("calculatorNumbers").addEventListener("click", (e) => {
-//   const pinCode = e.target.innerText;
-//   const secondInput = document.getElementById("secondInput");
+document.getElementById("calculatorNumbers").addEventListener("click", (e) => {
+  const pinCode = e.target.innerText;
+  const secondInput = document.getElementById("secondInput");
 
-//   if (isNaN(pinCode)) {
-//     if (pinCode == "C") {
-//       secondInput.value = "";
-//     }
-//   } else {
-//     const newPin = secondInput.value;
-//     const newPinCode = newPin + pinCode;
-//     secondInput.value = newPinCode;
-//   }
-// });
+  if (isNaN(pinCode)) {
+    if (pinCode == "C") {
+      secondInput.value = "";
+    }
+  } else {
+    const newPin = secondInput.value;
+    const newPinCode = newPin + pinCode;
+    secondInput.value = newPinCode;
+  }
+});
+// -----------
+document.getElementById("pinMatched").style.display = "none";
+document.getElementById("pinDon'tMatched").style.display = "none";
 
-// document.getElementById("submitBtn").addEventListener("click", () => {
-//     const firstInput = document.getElementById("firstInput");
-//     const secondInput = document.getElementById("secondInput");
-//     if(firstInput==secondInput){
+document.getElementById("submitBtn").addEventListener("click", () => {
+  const firstInput = document.getElementById("firstInput");
+  const newFirstInput = firstInput.value;
+  const secondInput = document.getElementById("secondInput");
+  const newSecondInput = secondInput.value;
 
-//     }
-// });
+  secondInput.value = "";
 
-// let count = 0;
-// console.log(parseInt(count + 1));
-
-// let num = Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9);
-
-// function generatePin() {
-//   return Math.floor(Math.random() * 90000) + 10000;
-// }
-// console.log(generatePin());
-
-// !isNaN('10X')
-
-const product = 5;
-const price = "7";
-const subTotal = product * price;
-console.log(subTotal);
-const tax = subTotal / 10;
-const total = subTotal + tax;
-console.log(total);
-
-const category = "pen";
-const performer = category + "-teller";
-console.log(performer);
+  if (newFirstInput == newSecondInput) {
+    console.log("matchrd");
+    document.getElementById("pinMatched").style.display = "block";
+  } else {
+    console.log("hiiiii");
+    document.getElementById("pinDon'tMatched").style.display = "block";
+  }
+});
