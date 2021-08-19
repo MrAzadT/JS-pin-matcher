1cac6e7bfe98e11d360201a4522e445b47ea9f5b
azadtamal <mrazadt@gmail.com>
2021-08-19 22:20:55 +0600

    app

--- app.js
+++ app.js
@@ -1,55 +1,52 @@
-// function generatePin() {
-//   let num = Math.floor(1000 + Math.random() * 9000);
+function generatePin() {
+  let num = Math.floor(1000 + Math.random() * 9000);
 
-//   const firstInput = document.getElementById("firstInput");
-//   firstInput.value = num;
-//   return firstInput;
-// }
+  const firstInput = document.getElementById("firstInput");
+  firstInput.value = num;
+  return firstInput;
+}
 
-// document.getElementById("calculatorNumbers").addEventListener("click", (e) => {
-//   const pinCode = e.target.innerText;
-//   const secondInput = document.getElementById("secondInput");
+document.getElementById("calculatorNumbers").addEventListener("click", (e) => {
+  const pinCode = e.target.innerText;
+  const secondInput = document.getElementById("secondInput");
 
-//   if (isNaN(pinCode)) {
-//     if (pinCode == "C") {
-//       secondInput.value = "";
-//     }
-//   } else {
-//     const newPin = secondInput.value;
-//     const newPinCode = newPin + pinCode;
-//     secondInput.value = newPinCode;
-//   }
-// });
-// // -----------
-// document.getElementById("pinMatched").style.display = "none";
-// document.getElementById("pinDon'tMatched").style.display = "none";
+  if (isNaN(pinCode)) {
+    if (pinCode == "C") {
+      secondInput.value = "";
+    }
+  } else {
+    const newPin = secondInput.value;
+    const newPinCode = newPin + pinCode;
+    secondInput.value = newPinCode;
+  }
+});
+// -----------
+document.getElementById("pinMatched").style.display = "none";
+document.getElementById("pinDon'tMatched").style.display = "none";
 
-// document.getElementById("submitBtn").addEventListener("click", () => {
-//   const firstInput = document.getElementById("firstInput");
-//   const newFirstInput = firstInput.value;
-//   const secondInput = document.getElementById("secondInput");
-//   const newSecondInput = secondInput.value;
+document.getElementById("submitBtn").addEventListener("click", () => {
+  const firstInput = document.getElementById("firstInput");
+  const newFirstInput = firstInput.value;
+  const secondInput = document.getElementById("secondInput");
+  const newSecondInput = secondInput.value;
 
-//   secondInput.value = "";
+  secondInput.value = "";
 
-//   const pinMatched = document.getElementById("pinMatched");
-//   pinMatched.style.display = "block";
-//   const pinDontMatched = document.getElementById("pinDon'tMatched");
-//   pinDontMatched.style.display = "block";
+  const pinMatched = document.getElementById("pinMatched");
+  pinMatched.style.display = "block";
+  const pinDontMatched = document.getElementById("pinDon'tMatched");
+  pinDontMatched.style.display = "block";
 
-//   if (newFirstInput == newSecondInput) {
-//     pinMatched.style.display = "block";
-//     pinDontMatched.style.display = "none";
-//   } else {
-//     pinMatched.style.display = "none";
-//     pinDontMatched.style.display = "block";
-//   }
-// });
+  if (newFirstInput == newSecondInput) {
+    pinMatched.style.display = "block";
+    pinDontMatched.style.display = "none";
+  } else {
+    pinMatched.style.display = "none";
+    pinDontMatched.style.display = "block";
+  }
+});
 
-// const task = "Practice Makes a person perfect";
+const task = "Practice Makes a person perfect";
 
-// const index = task.toLowerCase().indexOf("M");
-// console.log(index);
-
-// const str1 = "Bangladesh is a beautiful country";
-// console.log(str1.endsWith("country"));
+const index = task.toLowerCase().indexOf("M");
+console.log(index);
